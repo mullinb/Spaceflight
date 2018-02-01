f4bc3ad34d0f96816c6528691d149d7d086004bc
Brian Mullin <mullin.mm@gmail.com>
2018-02-01 16:44:49 +0100

    omggg

--- public/script.js
+++ public/script.js
@@ -8,8 +8,6 @@ var container, stats;
 var camera, controls, scene, renderer;
 
 
-
-
 var d, dPlanet, dMoon, dMoonVec = new THREE.Vector3();
 var clock = new THREE.Clock();
 
@@ -21,15 +19,29 @@ container.appendChild( renderer.domElement );
 
 
     scene = new Physijs.Scene;
+    scene.setGravity(new THREE.Vector3(0,-1000,0));
+
     var camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.8, 12600 * 100000)
 
-    camera.position.set( 60, 50, 60 );
-    camera.lookAt( scene.position );
-    scene.add( camera );
+    camera.position.set( 500, 500, 500 );
+
+    var gun = new THREE.Mesh(
+        new THREE.CylinderGeometry (40, 40, 300),
+        new THREE.MeshPhongMaterial({ color: 0x97ff03})
+    )
+
+    gun.position.z = -250;
+    gun.position.x = 120;
+    gun.position.y = 90;
+    gun.rotateX(Math.PI/2);
+
+    camera.add(gun)
+
+    scene.add(camera);
 
 
     var controls = new THREE.FlyControls(camera, container);
-    controls.movementSpeed = 1505.3;
+    controls.movementSpeed = 9505.3;
     controls.domElement = container;
     controls.rollSpeed = Math.PI / 6;
     controls.autoForward = false;
@@ -52,7 +64,7 @@ container.appendChild( renderer.domElement );
 
 //
 //     var geometry = new THREE.SphereGeometry( 10, 32, 32 );
-//     var material = new THREE.MeshPhongMaterial();
+//     var material = new THREE.MeshBasicMaterial();
 //     material.map = new THREE.TextureLoader().load('/earthmap4k.jpg');
 //     var earthMesh = new THREE.Mesh(geometry, material);
 //     earthMesh.rotation.y = 0,401426;
@@ -64,14 +76,14 @@ container.appendChild( renderer.domElement );
 //     scene.add( mesh );
 //
 //     var geometry = new THREE.SphereGeometry( 40, 32, 32 );
-//     var material = new THREE.MeshPhongMaterial( {color: 0xffed00, side: THREE.BackSide} );
+//     var material = new THREE.MeshBasicMaterial( {color: 0xffed00, side: THREE.BackSide} );
 //     var sun = new THREE.Mesh(geometry, material);
 //
 //     sun.position.set (200, 0, 200);
 //     scene.add( sun );
 //
 //     var geometry = new THREE.SphereGeometry( 4, 32, 32 );
-//     var material = new THREE.MeshPhongMaterial();
+//     var material = new THREE.MeshBasicMaterial();
 //     material.map = new THREE.TextureLoader().load('/moonmap4k.jpg');
 //     var moon = new THREE.Mesh(geometry, material);
 //
@@ -95,33 +107,103 @@ container.appendChild( renderer.domElement );
 // mesh.castShadow = true;
 // mesh.receiveShadow = true;
 
-var boxGeometry = new THREE.BoxGeometry( 10000, 10000, 10000 );
-var boxMaterial = new THREE.MeshPhongMaterial( { side: THREE.BackSide} );
-boxMaterial.map = new THREE.TextureLoader().load('/space2.jpg');
-var boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+var floorGeometry = new THREE.BoxGeometry( 25000, 250, 25000 );
+var floorMaterial = new THREE.MeshBasicMaterial( { side: THREE.BackSide} );
+floorMaterial.map = new THREE.TextureLoader().load('/space2.jpg');
+var floorMesh = new Physijs.ConvexMesh(floorGeometry, floorMaterial, 0);
+floorMesh.position.set( 0, -5000, 0);
 
-scene.add(boxMesh);
-var box = new Physijs.BoxMesh(
-    new THREE.CubeGeometry( 150, 150, 150 ),
-    new THREE.MeshBasicMaterial({ color: 0x888888 })
-);
-scene.add( box );
-
-var position = { x : 0, y: 0 }; var target = { x : 2000, y: 2000 };
-
-var tween = new TWEEN.Tween(position).to(target, 2000);
-
-tween.delay(500);
-
-tween.onUpdate(function(){
-    box.position.x = position.x;
-    box.position.y = position.y;
-    console.log(box.position);
+floorMesh.addEventListener( 'collision', function( other_object, relative_velocity, relative_rotation, contact_normal ) {
 });
 
-tween.start();
+var side1Geometry = new THREE.BoxGeometry( 250, 5000, 25000 );
+var side1Material = new THREE.MeshBasicMaterial( { side: THREE.BackSide} );
+side1Material.map = new THREE.TextureLoader().load('/space2.jpg');
+var side1Mesh = new Physijs.ConvexMesh(side1Geometry, side1Material, 0);
+side1Mesh.position.set(12500, -3500, 0);
+
+side1Mesh.addEventListener( 'collision', function( other_object, relative_velocity, relative_rotation, contact_normal ) {
+});
+
+var side2Geometry = new THREE.BoxGeometry( 25000, 5000, 250 );
+var side2Material = new THREE.MeshBasicMaterial( { side: THREE.BackSide} );
+side2Material.map = new THREE.TextureLoader().load('/space2.jpg');
+var side2Mesh = new Physijs.ConvexMesh(side2Geometry, side2Material, 0);
+side2Mesh.position.set( 0, -3500, 12500);
+
+side2Mesh.addEventListener( 'collision', function( other_object, relative_velocity, relative_rotation, contact_normal ) {
+});
+
+var side3Geometry = new THREE.BoxGeometry( 250, 5000, 25000 );
+var side3Material = new THREE.MeshBasicMaterial( { side: THREE.BackSide} );
+side3Material.map = new THREE.TextureLoader().load('/space2.jpg');
+var side3Mesh = new Physijs.ConvexMesh(side3Geometry, side3Material, 0);
+side3Mesh.position.set( -12500, -3500, 0);
+
+side3Mesh.addEventListener( 'collision', function( other_object, relative_velocity, relative_rotation, contact_normal ) {
+});
+
+var side4Geometry = new THREE.BoxGeometry( 25000, 5000, 250 );
+var side4Material = new THREE.MeshBasicMaterial( { side: THREE.BackSide} );
+side4Material.map = new THREE.TextureLoader().load('/space2.jpg');
+var side4Mesh = new Physijs.ConvexMesh(side4Geometry, side4Material, 0);
+side4Mesh.position.set( 0, -3500, -12500);
+
+side4Mesh.addEventListener( 'collision', function( other_object, relative_velocity, relative_rotation, contact_normal ) {
+});
 
 
+scene.add(floorMesh)
+scene.add(side1Mesh)
+scene.add(side2Mesh)
+scene.add(side3Mesh)
+scene.add(side4Mesh)
+
+
+var box = new Physijs.BoxMesh(
+    new THREE.CubeGeometry( 400, 400, 400 ),
+    new THREE.MeshPhongMaterial({ color: 0x97ff03}), 1
+);
+
+box.addEventListener( 'collision', function( other_object, relative_velocity, relative_rotation, contact_normal ) {
+    scene.remove(box);
+});
+
+box.setCcdMotionThreshold(5);
+box.setCcdSweptSphereRadius(1);
+
+scene.add( box );
+//
+// var position = { x : 0, y: 0 }; var target = { x : 0, y: -5000 };
+//
+//
+// var tweenA = new TWEEN.Tween(position).to({x: 100, y: -5000, z: 100}, 5000);
+//
+// var tweenB = new TWEEN.Tween(position).to({x: 0, y: 0, z: 0}, 5000);
+
+
+
+//
+// tweenA.onUpdate(function(){
+//     box.position.x = position.x;
+//     box.position.y = position.y;
+// });
+//
+// tweenB.onUpdate(function(){
+//     box.position.x = position.x;
+//     box.position.y = position.y;
+// });
+//
+//
+// tweenA.easing(TWEEN.Easing.Bounce.Out)
+//
+// tweenB.easing(TWEEN.Easing.Cubic.Out)
+//
+//
+// tweenA.chain(tweenB);
+// tweenB.chain(tweenA);
+
+// tweenA.start();
 
 // function moveCylinders() {
 //     for (let x=0; x<cylinder.length; x++) {
@@ -132,11 +214,11 @@ tween.start();
 // }
 
 var render = function() {
-    scene.simulate();
     var delta = clock.getDelta();
     controls.update(delta);
+    scene.simulate();
     // moveCylinders();
-    tween.update(delta);
+    // TWEEN.update();
 
     renderer.render(scene, camera)
     requestAnimationFrame(render)
@@ -145,105 +227,83 @@ var render = function() {
 
 var i = 0;
 var cylinder = []
-
-
+var bulletSpeed = 20000;
 
 var map = {};
-onkeydown = onkeyup = function(e){
-    e = e || event;
-    map[e.key] = e.type == 'keydown';
-//     if (map["a"]) {
-//         e.preventDefault();
-//         camera.rotateZ(3 * .0174533);
-//     }
-//     if (map["w"]) {
-//         e.preventDefault();
-//         controls.cameraPositionX += 15 * (camera.getWorldDirection().x);
-//         controls.cameraPositionY += 15 * (camera.getWorldDirection().y);
-//         controls.cameraPositionZ += 15 * (camera.getWorldDirection().z);
-//     }
-//     if (map["d"]) {
-//         e.preventDefault();
-//         camera.rotateZ(-3 * .0174533);
-//     }
-//     if (map["s"]) {
-//         e.preventDefault();
-//         controls.cameraPositionX -= 15 * (camera.getWorldDirection().x);
-//         controls.cameraPositionY -= 15 * (camera.getWorldDirection().y);
-//         controls.cameraPositionZ -= 15 * (camera.getWorldDirection().z);
-//     }
-//     if (map["ArrowLeft"]) {
-//         e.preventDefault();
-//         camera.rotateY(3 * .0174533)
-//
-//     }
-//     if (map["ArrowUp"]) {
-//         e.preventDefault();
-//         camera.rotateX(3 * .0174533)
-//     }
-//     if (map["ArrowRight"]) {
-//         e.preventDefault();
-//         camera.rotateY(3 * -.0174533)
-//     }
-//     if (map["ArrowDown"]) {
-//         e.preventDefault();
-//         camera.rotateX(3 * -.0174533)
-//     }
-    if (map[" "]) {
-        // e.preventDefault()
-        // var merged = new THREE.Geometry();
-        // var cyl = new THREE.CylinderGeometry(2, 2, 6);
-        // var top = new THREE.SphereGeometry(2);
-        // var bot = new THREE.SphereGeometry(2);
-        // var matrix2 = new THREE.Matrix4();
-        // matrix2.makeTranslation(0, 3, 0);
-        // top.applyMatrix(matrix2);
-        // var matrix2 = new THREE.Matrix4();
-        // matrix2.makeTranslation(0, -3, 0);
-        // bot.applyMatrix(matrix2);
-        // merged.merge(top);
-        // merged.merge(bot);
-        // merged.merge(cyl);
-        // var material = new THREE.MeshBasicMaterial( {color: 0xff0000} );
-        // // let d = new THREE.vector3();
-        // cylinder[i] = new Physijs.Mesh( merged, material );
-        // cylinder[i].position.x = camera.position.x;
-        // cylinder[i].position.y = camera.position.y;
-        // cylinder[i].position.z = camera.position.z;
 
+var allowed = true;
+var timer;
 
-        camera.lookAt(boxMesh);
-        // let tmpQuaternion = new THREE.Quaternion();
-        //
-        // tmpQuaternion.set( rotationVector.x * rotMult, this.rotationVector.y * rotMult, this.rotationVector.z * rotMult, 1 ).normalize();
-        // this.object.quaternion.multiply( this.tmpQuaternion );
+document.addEventListener('keydown', function(e) {
+  if (e.repeat != undefined) {
+    allowed = !e.repeat;
+  }
+  if (!allowed) return;
+  allowed = false;
+  if (e.key === " ") {
+      e.preventDefault()
+      generateBullet();
+      timer = setInterval(generateBullet, 166);
+  }
+});
 
-
-        // cylinder[i].rotation.x = camera.rotation.x;
-        // cylinder[i].rotation.y = camera.rotation.y;
-        // cylinder[i].rotation.z = camera.rotation.z;
-
-
-
-        // cylinder[i].vectorStore = {
-        //     x: camera.getWorldDirection().x * 10,
-        //     y: camera.getWorldDirection().y * 10,
-        //     z: camera.getWorldDirection().z * 10,
-        // };
-        // scene.add(cylinder[i]);
-//         console.log("hi");
-//         // var renderLaser = function() {
-//         //     requestAnimationFrame(render)
-//         //     cylinder[i].position.x += 30 * (camera.getWorldDirection().x);
-//         //     cylinder[i].position.y += 30 * (camera.getWorldDirection().y);
-//         //     cylinder[i].position.z += 30 * (camera.getWorldDirection().z);
-//         // }
-//         // renderLaser();
-        i++
+document.addEventListener('keyup', function(e) {
+    if (e.key === " ") {
+        clearInterval(timer);
+        allowed = true;
     }
-//     // if (map["space"])
-//
+});
+// $(document).focus(function(e) {
+//   allowed = true;
+// });
+
+function generateBullet () {
+
+    var merged = new THREE.Geometry();
+    var cyl = new THREE.CylinderGeometry(30, 30, 210);
+    var top = new THREE.CylinderGeometry(30, 20, 50);
+    var bot = new THREE.CylinderGeometry(30, 20, 50);
+    var matrix2 = new THREE.Matrix4();
+    matrix2.makeTranslation(0, 110, 0);
+    top.applyMatrix(matrix2);
+    var matrix2 = new THREE.Matrix4();
+    matrix2.makeTranslation(0, -130, 0);
+    bot.applyMatrix(matrix2);
+    merged.merge(top);
+    merged.merge(bot);
+    merged.merge(cyl);
+    var material = new THREE.MeshPhongMaterial( {color: 0xff0000} );
+    // let d = new THREE.vector3();
+    cylinder[i] = new Physijs.CapsuleMesh(merged, material, 1);
+    gun.add(cylinder[i]);
+    cylinder[i].position.x = gun.getWorldPosition().x;
+    cylinder[i].position.y = gun.getWorldPosition().y;
+    cylinder[i].position.z = gun.getWorldPosition().z;
+
+
+
+    // var quaternion = new THREE.Quaternion();
+    // quaternion.setFromAxisAngle(gun.getWorldDirection(), Math.PI/2);
+
+    cylinder[i].setRotationFromEuler(gun.getWorldRotation());
+    cylinder[i].translateY(-280);
+
+
+    cylinder[i].addEventListener('ready', function() {
+        let self = cylinder[i];
+        setTimeout(function() {
+            scene.remove( self );
+
+        }, 3000)
+        // this.applyCentralImpulse(new THREE.Vector3(camera.getWorldDirection().x * 100000000000, camera.getWorldDirection().y * 100000000000, camera.getWorldDirection().z * 100000000000));
+        var force = new THREE.Vector3(camera.getWorldDirection().x * bulletSpeed, camera.getWorldDirection().y * bulletSpeed, camera.getWorldDirection().z * bulletSpeed);
+        cylinder[i].applyCentralImpulse(force);
+        i++
+    });
+    scene.add(cylinder[i]);
 }
+
+
 
 function handleWindowResize() {
     // update height and width of the renderer and the camera
@@ -256,5 +316,4 @@ function handleWindowResize() {
 
 window.addEventListener('resize', handleWindowResize);
 
-window.onresize
 render();
